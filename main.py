576ccc429e1267ddb5796b93b65bee1d4cb298cc
Gael Perrin <gael.perrin90@gmail.com>
2024-07-15 18:35:30 +0200

    implemented simple lstm

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ class VolumeDataset(Dataset):
         ##How to do it ? Problem with outliers
 
         ###normalize
-        #self.scaler = MinMaxScaler(feature_range=(0, 1))
-        #self.train_dataset.loc[:, 'volume'] = self.scaler.fit_transform(self.train_dataset[['volume']])
-        #self.test_dataset.loc[:, 'volume'] = self.scaler.transform(self.test_dataset[['volume']])
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.train_dataset.loc[:, 'volume'] = self.scaler.fit_transform(self.train_dataset[['volume']])
+        self.test_dataset.loc[:, 'volume'] = self.scaler.transform(self.test_dataset[['volume']])
 
         #self.scaler.inverse_transform(self.total_volume_per_day_norm)[:, [0]]
 
@@ -61,38 +61,40 @@ class VolumeDataset(Dataset):
         if self.train:
             #all past Sn volumes
             output_dataset = self.train_dataset[idx:idx + self.Sn]
-            #period number k and date number d (as in instructions) used in model performance evaluation
-            period_k = output_dataset['k'].iloc[-1]
-            date_d = output_dataset['d'].iloc[-1]
-            volume = output_dataset['volume']
-            _last_date = output_dataset['date'].iloc[-1]
-            #note: missing intraday also include opening and closing
-            missing_intraday = 104 - len(output_dataset[output_dataset['date'] == _last_date])
-            total_volume = self.total_volume_per_day.loc[_last_date]
-            return [
-                torch.tensor(volume.values, dtype=torch.float32), 
-                torch.tensor(missing_intraday, dtype=torch.float32), 
-                torch.tensor(total_volume, dtype=torch.float32),
-                torch.tensor(period_k, dtype=torch.float32),
-                torch.tensor(date_d, dtype=torch.float32)
-                ]
         else:
             output_dataset = self.test_dataset[idx:idx + self.Sn]
-            #period number k and date number d (as in instructions) used in model performance evaluation
-            period_k = output_dataset['k'].iloc[-1]
-            date_d = output_dataset['d'].iloc[-1]
-            volume = output_dataset['volume']
-            _last_date = output_dataset['date'].iloc[-1]
-            #note: missing intraday also include opening and closing
-            missing_intraday = 104 - len(output_dataset[output_dataset['date'] == _last_date])
-            total_volume = self.total_volume_per_day.loc[_last_date]
-            return [
-                torch.tensor(volume.values, dtype=torch.float32), 
-                torch.tensor(missing_intraday, dtype=torch.float32), 
-                torch.tensor(total_volume, dtype=torch.float32),
-                torch.tensor(period_k, dtype=torch.float32),
-                torch.tensor(date_d, dtype=torch.float32)
-                ]
+        #period number k and date number d (as in instructions) used in model performance evaluation
+        period_k = output_dataset['k'].iloc[-1]
+        date_d = output_dataset['d'].iloc[-1]
+        volume = output_dataset['volume']
+        #_last_date = output_dataset['date'].iloc[-1]
+        #note: missing intraday also include opening and closing
+        #missing_intraday = 104 - len(output_dataset[output_dataset['date'] == _last_date])
+        #total_volume = self.total_volume_per_day.loc[_last_date]
+        return {
+            'volume' : torch.tensor(volume.values, dtype=torch.float32), 
+        }
+
+###
+#Models
+###
+
+class LSTM(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=10, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(10, 1)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.linear(x)
+        return x
+
+    def prepare_input(self, batch):
+        return torch.narrow(batch['volume'], 1, 0, batch['volume'].shape[1] - 1).to(device)
+
+    def prepare_target(self, batch):
+        return torch.narrow(batch['volume'], 1, batch['volume'].shape[1] - 1, 1).to(device)
 
 class TotalVolumeEstimator(nn.Module):
     def __init__(self, input_size):
@@ -106,13 +108,28 @@ class TotalVolumeEstimator(nn.Module):
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+    def prepare_input(self, batch):
+        missing_intraday = batch['missing_intraday'].to(device)
+        volume = batch['volume'].to(device)
+        input = torch.cat([volume, missing_intraday.unsqueeze(-1)], dim=1)
+        input = input.to(device)
+        return input
+    
+    def prepare_target(self, batch):
+        return batch['total_volume'].to(device).unsqueeze(-1)
+
+
 
 batch_size = 100
 past_input_n_days = 3
-Sn = past_input_n_days*104 + 1
-
-input_size = Sn + 1  # 3*104 volumes + 1 missing intraday count
-model = TotalVolumeEstimator(input_size).to(device)
+#Sn = past_input_n_days*104 + 1
+#input_size = Sn + 1  # 3*104 volumes + 1 missing intraday count
+#model = TotalVolumeEstimator(input_size).to(device)
+Sn = past_input_n_days*104 + 2 #past record + current record + unknown (to be predicted record)
+#Sn  =  2
+input_size = Sn - 1  # 3*104 volumes + 1 missing intraday count
+model = LSTM(input_size).to(device)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
@@ -121,7 +138,7 @@ train_loader = DataLoader(train, batch_size=batch_size, shuffle=True)
 test = VolumeDataset('dataKCx.csv', Sn=Sn, train=False, last_training_date="2021-5-08")
 test_loader = DataLoader(test, batch_size=batch_size, shuffle=True)
 
-num_epochs=20
+num_epochs=100
 
 ###
 #TRAINING
@@ -130,39 +147,39 @@ num_epochs=20
 def train(data_loader, model, loss_fn, optimizer):
     model.train()
     loss_list = []
-    for (volume, missing_intraday, total_volume, period_k, date_d) in train_loader:
-        volume = volume.to(device)
-        missing_intraday = missing_intraday.to(device)
-        total_volume = total_volume.to(device)
-        input = torch.cat([volume, missing_intraday.unsqueeze(-1)], dim=1)
-        input = input.to(device)
-        predicted_total_volume = model(input)
-        loss = loss_fn(predicted_total_volume, total_volume.unsqueeze(-1))
+    import time
+    start = time.time()
+    for batch in data_loader:
+        prediction = model(model.prepare_input(batch))
+        target = model.prepare_target(batch)
+        loss = loss_fn(prediction, target)
         loss_list.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        print(f'Took {time.time() - start}')
+        start = time.time()
     average_loss = sum(loss_list)/len(loss_list)
     return average_loss
 
 def test(data_loader, model, loss_fn):
     model.eval()
     loss_list = []
-    for (volume, missing_intraday, total_volume, period_k, date_d) in test_loader:
-        volume = volume.to(device)
-        missing_intraday = missing_intraday.to(device)
-        total_volume = total_volume.to(device)
-        input = torch.cat([volume, missing_intraday.unsqueeze(-1)], dim=1)
-        input = input.to(device)
-        predicted_total_volume = model(input)
-        loss = loss_fn(predicted_total_volume, total_volume.unsqueeze(-1))
-        loss_list.append(loss.item())
-    average_loss = sum(loss_list)/len(loss_list)
-    return average_loss
+    with torch.no_grad():
+        for batch in data_loader:
+            prediction = model(model.prepare_input(batch))
+            target = model.prepare_target(batch)
+            loss = loss_fn(prediction, target)
+            #predicted_total_volume = model(model.prepare_input(batch))
+            #total_volume = batch['total_volume'].to(device)
+            #loss = loss_fn(predicted_total_volume, total_volume.unsqueeze(-1))
+            loss_list.append(loss.item())
+        average_loss = sum(loss_list)/len(loss_list)
+        return average_loss
 
 for epoch in range(num_epochs):
     average_loss_train = train(train_loader, model, criterion, optimizer)
-    average_loss_test = test(train_loader, model, criterion)
+    average_loss_test = test(test_loader, model, criterion)
     print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {average_loss_train}, Test Loss: {average_loss_test}")
 print('pass')
 
